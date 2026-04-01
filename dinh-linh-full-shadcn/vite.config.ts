bf7728de1a6f1105425cdceec0dc9697ba537e66
Tô Thiên Long <110765431+thienlong25@users.noreply.github.com>
2026-04-01 10:18:19 +0700

    Update vite.config.ts

--- dinh-linh-full-shadcn/vite.config.ts
+++ dinh-linh-full-shadcn/vite.config.ts
@@ -1,4 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
-export default defineConfig({ plugins:[react()], resolve:{ alias:{ "@": path.resolve(__dirname, "./src") } } });
+import { fileURLToPath, URL } from "node:url";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
